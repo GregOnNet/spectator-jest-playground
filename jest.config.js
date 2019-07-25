fe9f13f5fcafb743d888e00fa90a1db3669597ac
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-07-25 11:25:55 +0200

    test: setup jest

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+const esModules = [].join('|');
+
+module.exports = {
+  globals: {
+    'ts-jest': {
+      allowSyntheticDefaultImports: true
+    }
+  },
+  transformIgnorePatterns: [`<rootDir>/node_modules/(?!${esModules})`],
+
+  transform: {
+    '^.+\\.js$': 'babel-jest'
+  }
+};
