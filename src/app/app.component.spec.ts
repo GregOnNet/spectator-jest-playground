55e233919bb4c70e41767009ba30a54c5d0e2739
Gregor Woiwode <gregor.woiwode@gmail.com>
2019-07-25 11:37:20 +0200

    broken: use spectator causing ts error

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,11 @@
-import { async, TestBed } from '@angular/core/testing';
+import { async } from '@angular/core/testing';
+import { createTestComponentFactory } from '@netbasal/spectator/jest';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [AppComponent]
-    }).compileComponents();
-  }));
+  const createComponent = createTestComponentFactory(AppComponent);
+
+  beforeEach(async(() => createComponent()));
 
   it.each([[1], [2]])('should work %s', () => {});
 });
